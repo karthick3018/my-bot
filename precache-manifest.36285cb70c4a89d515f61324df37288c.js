0b53928092b5bbbdeade2ce7c603d8ad072bbfc9
karthick3018 <karthickraja3018@gmail.com>
2020-10-11 00:07:48 +0530

    Updates

---
+++ precache-manifest.36285cb70c4a89d515f61324df37288c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a616e2353f9898a39650f6ac6f2e63b",
+    "url": "/my-bot/index.html"
+  },
+  {
+    "revision": "bd870a22a61b50d9e27b",
+    "url": "/my-bot/static/css/main.512e390c.chunk.css"
+  },
+  {
+    "revision": "b3481f96fcdf2ae34d27",
+    "url": "/my-bot/static/js/2.5a4f36df.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/my-bot/static/js/2.5a4f36df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd870a22a61b50d9e27b",
+    "url": "/my-bot/static/js/main.7c528bd8.chunk.js"
+  },
+  {
+    "revision": "393a2aa8c3acc6539b5e",
+    "url": "/my-bot/static/js/runtime-main.11cc6783.js"
+  }
+]);
